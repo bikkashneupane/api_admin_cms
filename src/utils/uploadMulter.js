2ea20fb7371ef48264381ca0ddb9800979b65fa4
bikashneupane17 <edubikkash@gmail.com>
2024-07-23 23:04:24 +1000

    cloudinary cloud server configured to save images

--- src/utils/uploadMulter.js
+++ src/utils/uploadMulter.js
@@ -1,27 +1,52 @@
+// import multer from "multer";
+
+// const imgFolderPath = "public/img/product";
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     let error = null;
+//     //
+//     cb(error, imgFolderPath);
+//   },
+//   filename: (req, file, cb) => {
+//     let error = "";
+//     // make a unique name
+//     const fullFileName = Date.now() + "-" + file.originalname;
+//     cb(error, fullFileName);
+//   },
+// });
+
+// const limits = {
+//   fileSize: 10 * 1024 * 1024, // limit fileseize to 10 MB
+// };
+
+// // const fileFilter =
+
+// const multerUpload = multer({ storage, limits });
+
+// export default multerUpload;
+
 import multer from "multer";
+import cloudinary from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
-const imgFolderPath = "public/img/product";
+// configure cloudinary
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    let error = null;
-    //
-    cb(error, imgFolderPath);
-  },
-  filename: (req, file, cb) => {
-    let error = "";
-    // make a unique name
-    const fullFileName = Date.now() + "-" + file.originalname;
-    cb(error, fullFileName);
+// configure multer-storage-cloudinary
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary.v2,
+  params: {
+    folder: "vikiasmy-watches",
+    allowed_formats: ["jpg", "png", "gif", "webp"],
   },
 });
 
-const limits = {
-  fileSize: 10 * 1024 * 1024, // limit fileseize to 10 MB
-};
-
-// const fileFilter =
-
-const multerUpload = multer({ storage, limits });
+// multer upload
+const multerUpload = multer({ storage });
 
 export default multerUpload;
